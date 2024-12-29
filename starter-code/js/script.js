cbbbb2cd9c2720da7b6c46622c8025f3bcea644a
JGedd <jgeddis08@gmail.com>
2024-12-29 22:41:49 +0000

    Initial commit

---
+++ starter-code/js/script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('form');
+
+// Show input error message
+function showError(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input);
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  checkEmail(email);
+});
